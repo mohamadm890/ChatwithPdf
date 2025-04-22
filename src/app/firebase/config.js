05d101dfdf83d6ca0a674574eb8e4e840f7d248c
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2025-04-22 10:20:35 +0100

    Update config.js

--- src/app/firebase/config.js
+++ src/app/firebase/config.js
@@ -2,7 +2,7 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBIWFM2LwiJjFeBClRwLz7qq4r1-yoG0S0",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
